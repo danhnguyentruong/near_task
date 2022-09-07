814e460fec8d72e4b31c9a1d7c52687cce8019b4
danhnguyentruong <danhnguyentruong495@gmail.com>
2022-09-07 06:46:30 +0200

    Add Task 14

---
+++ task14/backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DATE=$(date +%Y-%m-%d-%H-%M)
+DATADIR=.near
+BACKUPDIR=$HOME/backups/
+
+mkdir $BACKUPDIR
+
+sudo systemctl stop neard.service
+
+wait
+
+echo "NEAR node was stopped" | ts
+
+if [ -d "$BACKUPDIR" ]; then
+    echo "Backup started" | ts
+    echo "Compressing backup...."
+    tar -zcvf /root/backups/near_${DATE}.tar.gz -C $HOME/.near/data .
+
+    # cp -rf $DATADIR/data/ ${BACKUPDIR}/
+
+    # Submit backup completion status, you can use healthchecks.io, betteruptime.com or other services
+    # Example
+    curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/24b52f21-aac5-4997-855e-270665441327
+
+    echo "Backup completed" | ts
+    
+else
+    echo $BACKUPDIR is not created. Check your permissions.
+    exit 0
+fi
+
+sudo systemctl start neard.service
+
+echo "NEAR node was started" | ts
